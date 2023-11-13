56dce9caa4c2d98b58f42edd5b610280ee1ad1ec
dreamjz <25699818+dreamjz@users.noreply.github.com>
2023-11-13 19:37:30 +0800

    feat: Log time and err of retrieving version(s)
    Log the time and error of retrieving version(s) for command generate and update

--- pkg/versiongetter/fuzzy_getter.go
+++ pkg/versiongetter/fuzzy_getter.go
@@ -3,6 +3,7 @@ package versiongetter
 import (
 	"context"
 	"strings"
+	"time"
 
 	"github.com/aquaproj/aqua/v2/pkg/config/registry"
 	"github.com/aquaproj/aqua/v2/pkg/fuzzyfinder"
@@ -26,15 +27,20 @@ type FuzzyFinder interface {
 	FindMulti(items []*fuzzyfinder.Item, hasPreview bool) ([]int, error)
 }
 
-func (g *FuzzyGetter) Get(ctx context.Context, _ *logrus.Entry, pkg *registry.PackageInfo, currentVersion string, useFinder bool, limit int) string { //nolint:cyclop
+func (g *FuzzyGetter) Get(ctx context.Context, logE *logrus.Entry, pkg *registry.PackageInfo, currentVersion string, useFinder bool, limit int) string { //nolint:cyclop
 	filters, err := createFilters(pkg)
 	if err != nil {
 		return ""
 	}
 
+	const getVerTimeInfo string = "Retrieve pkg version(s) in "
+	const getVerErrWarn string = "Ver retrieving err"
+	start := time.Now()
 	if useFinder { //nolint:nestif
 		versions, err := g.getter.List(ctx, pkg, filters, limit)
+		logE.WithField("package", pkg.RepoOwner+"/"+pkg.RepoName).Info(getVerTimeInfo, time.Since(start))
 		if err != nil {
+			logE.WithField("package", pkg.RepoOwner+"/"+pkg.RepoName).WithError(err).Warn(getVerErrWarn)
 			return ""
 		}
 		if versions == nil {
@@ -61,7 +67,9 @@ func (g *FuzzyGetter) Get(ctx context.Context, _ *logrus.Entry, pkg *registry.Pa
 	}
 
 	version, err := g.getter.Get(ctx, pkg, filters)
+	logE.WithField("package", pkg.RepoOwner+"/"+pkg.RepoName).Info(getVerTimeInfo, time.Since(start))
 	if err != nil {
+		logE.WithField("package", pkg.RepoOwner+"/"+pkg.RepoName).WithError(err).Warn(getVerErrWarn)
 		return ""
 	}
 	return version
